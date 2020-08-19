8dfccff74379e0152084456188c1cededba73a3a
Eytyy <e.tayyem@gmail.com>
2020-08-19 15:18:34 +0300

    first commit

--- web/client-config.js
+++ web/client-config.js
@@ -1,6 +1,9 @@
 module.exports = {
   sanity: {
-    projectId: process.env.GATSBY_SANITY_PROJECT_ID || 'uj2a9mdf',
-    dataset: process.env.GATSBY_SANITY_DATASET || 'production'
+    projectId: process.env.GATSBY_SANITY_PROJECT_ID || "6r5nbdps",
+    dataset: process.env.GATSBY_SANITY_DATASET || "production",
+    token:
+      process.env.SANITY_PREVIEW_TOKEN ||
+      "skwAQ9QCs1xp52u45sla329fDdZC7Puqn6h3ygwIXqLJTAabWCiAPiXlFVvWIgv3rSDjcNmDtaCwmOx0XjZQqRPA7EovqVnCQFK1gmwMZddDonASrXNpG3kVAz0T9QpHtNoOf3u530e47pzV6drAXgfPd1gX6BDCCBxigYOpiuHAdo1hBX11"
   }
-}
+};
