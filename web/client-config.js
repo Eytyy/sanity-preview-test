3c067c00a70ebfdb54681dadaa2e932e5dd81fde
Eytyy <e.tayyem@gmail.com>
2020-08-19 15:50:54 +0300

    update token

--- web/client-config.js
+++ web/client-config.js
@@ -3,7 +3,7 @@ module.exports = {
     projectId: process.env.GATSBY_SANITY_PROJECT_ID || "6r5nbdps",
     dataset: process.env.GATSBY_SANITY_DATASET || "production",
     token:
-      process.env.SANITY_PREVIEW_TOKEN ||
+      process.env.SANITY_READ_TOKEN ||
       "skwAQ9QCs1xp52u45sla329fDdZC7Puqn6h3ygwIXqLJTAabWCiAPiXlFVvWIgv3rSDjcNmDtaCwmOx0XjZQqRPA7EovqVnCQFK1gmwMZddDonASrXNpG3kVAz0T9QpHtNoOf3u530e47pzV6drAXgfPd1gX6BDCCBxigYOpiuHAdo1hBX11"
   }
 };
